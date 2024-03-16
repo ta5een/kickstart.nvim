9b917ffc5ec486ba88d209cfc295a889f158b611
Ta-Seen <taseen00.islam@gmail.com>
2024-03-16 23:15:39 +1100

    feat(ui): configure `gitsigns` gutter
    Remove custom signs as I find the default signs aesthetically pleasing
    and less distracting.
    
    Set `signcolumn = true` to display signs in the gutter. This is the
    default value. Also set `numhl = true` to change the line number colors
    of changed lines.

--- init.lua
+++ init.lua
@@ -186,13 +186,8 @@ require('lazy').setup({
   { -- Adds git related signs to the gutter, as well as utilities for managing changes
     'lewis6991/gitsigns.nvim',
     opts = {
-      signs = {
-        add = { text = '+' },
-        change = { text = '~' },
-        delete = { text = '_' },
-        topdelete = { text = '‾' },
-        changedelete = { text = '~' },
-      },
+      signcolumn = true,
+      numhl = true,
     },
   },
 
