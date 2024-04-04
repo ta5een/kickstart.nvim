7a6943f1a0d3036999a093fdab1830e1bb4c558e
Ta-Seen <taseen00.islam@gmail.com>
2024-04-04 21:58:32 +1100

    feat(plugin): install `nvim-treesitter-context`

---
+++ lua/custom/plugins/treesitter-context.lua
@@ -0,0 +1,6 @@
+--- Show code context (lightweight alternative to context.vim)
+--- https://github.com/nvim-treesitter/nvim-treesitter-context
+--- @type LazySpec
+return {
+  'nvim-treesitter/nvim-treesitter-context',
+}
