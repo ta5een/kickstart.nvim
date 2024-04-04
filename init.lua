c884cb34ad961585595fdc6f83779318001656a1
Ta-Seen <taseen00.islam@gmail.com>
2024-04-04 21:55:31 +1100

    feat(plugin): install `zen-mode.nvim`

--- init.lua
+++ init.lua
@@ -903,6 +903,7 @@ require('lazy').setup({
   -- Or use telescope!
   -- In normal mode type `<space>sh` then write `lazy.nvim-plugin`
   -- you can continue same window with `<space>sr` which resumes last telescope search
+  { import = 'custom.plugins' },
 }, {
   ui = {
     -- If you are using a Nerd Font: set icons to an empty table which will use the
