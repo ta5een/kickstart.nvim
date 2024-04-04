6fa868cfdf5a5bc38cc1de5c9c82c875d7cf23b3
Ta-Seen <taseen00.islam@gmail.com>
2024-04-04 22:24:44 +1100

    feat(plugin): install `trouble.nvim`

---
+++ lua/custom/plugins/trouble.lua
@@ -0,0 +1,9 @@
+--- 🚦 A pretty diagnostics, references, telescope results, quickfix and
+--- location list to help you solve all the trouble your code is causing.
+--- https://github.com/folke/trouble.nvim
+--- @type LazySpec
+return {
+  'folke/trouble.nvim',
+  dependencies = { 'nvim-tree/nvim-web-devicons' },
+  opts = {},
+}
