eb336f9391ee35e90d7aa0d446a71ce4de1f33d2
Ta-Seen <taseen00.islam@gmail.com>
2024-04-04 21:48:15 +1100

    chore: fix formatting

--- init.lua
+++ init.lua
@@ -716,8 +716,12 @@ require('lazy').setup({
     end,
   },
 
-  -- Highlight todo, notes, etc in comments
-  { 'folke/todo-comments.nvim', event = 'VimEnter', dependencies = { 'nvim-lua/plenary.nvim' }, opts = { signs = false } },
+  { -- Highlight todo, notes, etc in comments
+    'folke/todo-comments.nvim',
+    event = 'VimEnter',
+    dependencies = { 'nvim-lua/plenary.nvim' },
+    opts = { signs = false },
+  },
 
   { -- Collection of various small independent plugins/modules
     'echasnovski/mini.nvim',
