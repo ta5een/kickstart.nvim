306bc19c1c76e00f69d3e1b704af11e3658160c1
Ta-Seen <taseen00.islam@gmail.com>
2024-04-04 21:55:31 +1100

    feat(plugin): install `zen-mode.nvim`

---
+++ lua/custom/plugins/zen-mode.lua
@@ -0,0 +1,11 @@
+--- 🧘 Distraction-free coding for Neovim
+--- https://github.com/folke/zen-mode.nvim
+--- @type LazySpec
+return {
+  'folke/zen-mode.nvim',
+  opts = {
+    window = {
+      width = 120,
+    },
+  },
+}
