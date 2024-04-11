1391141e9be4bf5df2796c64f427c9ec892e1ea0
Ta-Seen <taseen00.islam@gmail.com>
2024-04-12 02:05:23 +1000

    chore(ui): configure `treesitter-context` colours

--- lua/custom/plugins/treesitter-context.lua
+++ lua/custom/plugins/treesitter-context.lua
@@ -3,4 +3,13 @@
 --- @type LazySpec
 return {
   'nvim-treesitter/nvim-treesitter-context',
+  --- @type TSContext.UserConfig
+  opts = {
+    multiline_threshold = 1,
+    mode = 'topline',
+  },
+  init = function()
+    vim.cmd.hi 'TreesitterContextBottom gui=underline guisp=Grey'
+    vim.cmd.hi 'TreesitterContextLineNumberBottom gui=underline guisp=Grey'
+  end,
 }
