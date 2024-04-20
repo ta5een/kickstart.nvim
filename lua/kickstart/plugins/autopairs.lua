4a5adedf1b80a6de539c7729bac16e9f02e0929d
Ta-Seen <taseen00.islam@gmail.com>
2024-04-20 23:45:25 +1000

    chore(plugin): use `kickstart.plugins.autopairs`
    Since `kickstart.plugins.autopairs` does almost the exact same thing as
    `custom.plugins.autopairs`, I've decided to remove my implementation.

--- lua/kickstart/plugins/autopairs.lua
+++ lua/kickstart/plugins/autopairs.lua
@@ -1,6 +1,6 @@
--- autopairs
--- https://github.com/windwp/nvim-autopairs
-
+--- Autopairs for Neovim written in Lua
+--- https://github.com/windwp/nvim-autopairs
+--- @type LazySpec
 return {
   'windwp/nvim-autopairs',
   event = 'InsertEnter',
