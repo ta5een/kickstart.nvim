bf20b90d082e952f12f80cf36a5bf1ff4f511569
Ta-Seen <taseen00.islam@gmail.com>
2024-04-28 21:42:21 +1000

    feat(plugin): install `nvim-ts-autotag`

--- init.lua
+++ init.lua
@@ -943,11 +943,20 @@ require('lazy').setup({
     version = '^0.9.2',
     build = ':TSUpdate',
     main = 'nvim-treesitter.configs', -- Sets main module to use for opts
+    dependencies = {
+      'windwp/nvim-ts-autotag',
+    },
     -- [[ Configure Treesitter ]] See `:help nvim-treesitter`
     opts = {
       ensure_installed = { 'bash', 'c', 'diff', 'html', 'lua', 'luadoc', 'markdown', 'markdown_inline', 'query', 'vim', 'vimdoc' },
       -- Autoinstall languages that are not installed
       auto_install = true,
+      autotag = {
+        enable = true,
+        enable_rename = true,
+        enable_close = true,
+        enable_close_on_slash = true,
+      },
       highlight = {
         enable = true,
         -- Some languages depend on vim's regex highlighting system (such as Ruby) for indent rules.
