535cebb3f58d110a47535d126748dddf9ed8ea37
Ta-Seen <taseen00.islam@gmail.com>
2024-04-28 21:44:56 +1000

    chore(lang): configure pre-installed treesitter parsers

--- init.lua
+++ init.lua
@@ -841,7 +841,22 @@ require('lazy').setup({
       'windwp/nvim-ts-autotag',
     },
     opts = {
-      ensure_installed = { 'bash', 'c', 'diff', 'html', 'lua', 'luadoc', 'markdown', 'vim', 'vimdoc' },
+      ensure_installed = {
+        'bash',
+        'c',
+        'diff',
+        'html',
+        'javascript',
+        'lua',
+        'luadoc',
+        'markdown',
+        'markdown_inline',
+        'rust',
+        'tsx',
+        'typescript',
+        'vim',
+        'vimdoc',
+      },
       -- Autoinstall languages that are not installed
       auto_install = true,
       autotag = {
