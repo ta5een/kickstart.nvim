92ed39fde6852fa057d1ffdaf27d4ec76be13bc8
Ta-Seen <taseen00.islam@gmail.com>
2024-05-06 14:16:10 +1000

    feat(plugin): install `rainbow-delimiters.nvim`

---
+++ lua/custom/plugins/rainbow-delimiters.lua
@@ -0,0 +1,7 @@
+--- Rainbow delimiters for Neovim with Tree-sitter
+--- https://gitlab.com/HiPhish/rainbow-delimiters.nvim
+--- @type LazySpec
+return {
+  'HiPhish/rainbow-delimiters.nvim',
+  version = '^0.3.0',
+}
