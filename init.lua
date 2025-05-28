941941d97221984c07ab9dbb98bf29229e8c2f66
Ta-Seen <taseen00.islam@gmail.com>
2025-05-28 21:00:33 +1000

    fix(plugin): `nvim-treesitter` fails to work with Neovim v0.11.1
    Occassionally, treesitter functionality stops working when I upgrade
    Neovim and treesitter with Homebrew. This might be because I've
    hard-coded the version of this plugin, which may not be compatible with
    the newer versions of Neovim.
    
    I'll need to make sure to manually update this plugin whenever I upgrade
    Neovim and treesitter again.

--- init.lua
+++ init.lua
@@ -1009,7 +1009,6 @@ require('lazy').setup({
 
   { -- Highlight, edit, and navigate code
     'nvim-treesitter/nvim-treesitter',
-    version = '^0.9.2',
     build = ':TSUpdate',
     main = 'nvim-treesitter.configs', -- Sets main module to use for opts
     dependencies = {
